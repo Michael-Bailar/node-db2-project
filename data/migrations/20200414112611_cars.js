c64562abfa33a2dcb460d81c36a970a93cfc59ab
Michael-B1764 <mikebailar@gmail.com>
2020-04-14 11:41:04 -0700

    finished creating database/knex migration. Just need to configure the post request and do stretch

--- data/migrations/20200414112611_cars.js
+++ data/migrations/20200414112611_cars.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments('id')
+      tbl.string("VIN").notNullable().unique().index()
+      tbl.string("make").notNullable().index()
+      tbl.string("model").notNullable().index()
+      tbl.integer("mileage").notNullable()
+      tbl.string("type").index()
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('cars')
 };
